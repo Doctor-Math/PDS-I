eaa41c74ea605a2a3dd64aa949ef0c6e666a5bb5
Matheus Soares <92276485+VariableMath@users.noreply.github.com>
2024-08-15 17:03:44 -0300

    Add files via upload

---
+++ 02.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int n1, n2;
+int main(){
+    printf("Digite um número inteiro:");
+    scanf("%i",&n1);
+    printf("Digite outro número inteiro:");
+    scanf("%i",&n2);
+    printf("%i\n%i",n1,n2);
+}
