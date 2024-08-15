8d7641cde2888d17cfb70db6e06b82c29d2f89da
Matheus Soares <92276485+VariableMath@users.noreply.github.com>
2024-08-15 17:17:02 -0300

    Create Count.c

---
+++ Count.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(){
+    int n=233;
+    do{
+      printf("%i \n",n);  
+      n+=5;  
+    }while(n<=300);
+    do{
+      printf("%i \n",n);  
+      n+=3;    
+    }while(n<400);
+    do{
+      printf("%i \n",n);  
+      n+=5;  
+    }while(n<=457);
+}
